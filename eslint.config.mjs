616d94aa623e6b32b44ccb4bb6d335bfca18d46e
kazymir.rabier <kazymir.rabier@gmail.com>
2024-12-30 19:16:52 +0800

    fix pages job

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,4 @@ import tseslint from 'typescript-eslint';
 export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.recommended,
-  {
-    ignores: ["src/components/magicui/**/*.tsx"],
-  }
 );
