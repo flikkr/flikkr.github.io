b96872f1b347c8b36898768decd8d1854fd5f8b9
Kazymir Rabier <kazymir.rabier@gmail.com>
2025-02-03 18:16:51 +0800

    remove lint issue

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -13,10 +13,10 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <div className="pointer-events-none fixed inset-x-0 bottom-0 z-30 mx-auto mb-4 flex origin-bottom h-full max-h-14">
-      <div className="fixed bottom-0 inset-x-0 h-16 w-full bg-background to-transparent backdrop-blur-lg [-webkit-mask-image:linear-gradient(to_top,black,transparent)] dark:bg-background"></div>
-      <Dock className="z-50 pointer-events-auto relative mx-auto flex min-h-full h-full items-center px-1 bg-background [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)] transform-gpu dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset] ">
-        {DATA.navbar.map((item) => (
+    <div className='pointer-events-none fixed inset-x-0 bottom-0 z-30 mx-auto mb-4 flex origin-bottom h-full max-h-14'>
+      <div className='fixed bottom-0 inset-x-0 h-16 w-full bg-background to-transparent backdrop-blur-lg [-webkit-mask-image:linear-gradient(to_top,black,transparent)] dark:bg-background'></div>
+      <Dock className='z-50 pointer-events-auto relative mx-auto flex min-h-full h-full items-center px-1 bg-background [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)] transform-gpu dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset] '>
+        {DATA.navbar.map((item: any) => (
           <DockIcon key={item.href}>
             <Tooltip>
               <TooltipTrigger asChild>
@@ -27,7 +27,7 @@ export default function Navbar() {
                     "size-12"
                   )}
                 >
-                  <item.icon className="size-4" />
+                  <item.icon className='size-4' />
                 </Link>
               </TooltipTrigger>
               <TooltipContent>
@@ -36,7 +36,9 @@ export default function Navbar() {
             </Tooltip>
           </DockIcon>
         ))}
-        {DATA.navbar.length > 0 && <Separator orientation="vertical" className="h-full" />}
+        {DATA.navbar.length > 0 && (
+          <Separator orientation='vertical' className='h-full' />
+        )}
         {Object.entries(DATA.contact.social)
           .filter(([_, social]) => social.navbar)
           .map(([name, social]) => (
@@ -50,7 +52,7 @@ export default function Navbar() {
                       "size-12"
                     )}
                   >
-                    <social.icon className="size-4" />
+                    <social.icon className='size-4' />
                   </Link>
                 </TooltipTrigger>
                 <TooltipContent>
@@ -59,7 +61,7 @@ export default function Navbar() {
               </Tooltip>
             </DockIcon>
           ))}
-        <Separator orientation="vertical" className="h-full py-2" />
+        <Separator orientation='vertical' className='h-full py-2' />
         <DockIcon>
           <Tooltip>
             <TooltipTrigger asChild>
